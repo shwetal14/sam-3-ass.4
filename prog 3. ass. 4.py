f5c7cfffc46e2635c4f5f588845eea338393f97f
shwetal14 <95999701+shwetal14@users.noreply.github.com>
2022-01-04 15:54:16 +0530

    Add files via upload

---
+++ prog 3. ass. 4.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def square(n):
+    return n*n
+my_list = [4, 5, 2, 9]
+
+updated_list = map(square, my_list)
+print(list(updated_list))
+
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
